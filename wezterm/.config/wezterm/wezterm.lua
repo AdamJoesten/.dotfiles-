b178809b045c23a0517355318235e95811a918ca
Adam Joesten <adamjoesten@gmail.com>
2023-11-04 16:24:32 -0400

    [refactor] Update term styling

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,9 +4,11 @@ local config = {}
 -- Colors
 config.color_scheme = "tokyonight_night"
 -- Font
-config.font = wezterm.font_with_fallback({
-	"IosevkaTerm Nerd Font Mono",
-})
-config.harfbuzz_features = { "zero" }
+config.font = wezterm.font("IosevkaTerm NFM")
+config.hide_tab_bar_if_only_one_tab = true
+config.show_tab_index_in_tab_bar = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.window_decorations = "RESIZE"
 
 return config
