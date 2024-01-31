33fbd44c3c0010d5bd2c29550f8a87b802894704
Adam J <59425245+AdamJoesten@users.noreply.github.com>
2024-01-31 15:39:17 -0500

    Update wezterm.lua
    Revert styles, add 0xProto font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,11 +4,6 @@ local config = {}
 -- Colors
 config.color_scheme = "tokyonight_night"
 -- Font
-config.font = wezterm.font("IosevkaTerm NFM")
-config.hide_tab_bar_if_only_one_tab = true
-config.show_tab_index_in_tab_bar = false
-config.tab_bar_at_bottom = true
-config.use_fancy_tab_bar = false
-config.window_decorations = "RESIZE"
+config.font = wezterm.font("0xProto Nerd Font")
 
 return config
