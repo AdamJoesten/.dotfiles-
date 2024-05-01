3b386b9476e7993aa738566a6eeacc3db3541558
Adam Joesten <adamjoesten@gmail.com>
2024-05-01 08:45:43 -0400

    <refactor>: Enable nerd font, clean up comments

--- zsh/.oh-my-zsh/custom/alias.zsh
+++ zsh/.oh-my-zsh/custom/alias.zsh
@@ -1,3 +1,3 @@
-alias nvim="$HOME/.local/bin/nvim.appimage"
+alias cb="clipboard"
 alias obsidian="$HOME/.local/bin/obsidian.AppImage"
 alias firefox-developer-edition="$HOME/.local/bin/firefox-developer-edition"
