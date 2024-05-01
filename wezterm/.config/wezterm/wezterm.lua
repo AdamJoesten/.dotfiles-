52b009417563aea4b53d3600c38fe6981b0f2601
Adam Joesten <adamjoesten@gmail.com>
2024-05-01 08:28:40 -0400

    <refactor>: Monospace font, add comments

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,7 +4,8 @@ local config = {}
 -- Colors
 config.color_scheme = "tokyonight_night"
 -- Font
-config.font = wezterm.font("0xProto Nerd Font")
+config.font = wezterm.font("0xProto Nerd Font Mono")
+-- Application window
 config.window_decorations = "RESIZE" -- Remove title bar, enable window resize
 
 return config
