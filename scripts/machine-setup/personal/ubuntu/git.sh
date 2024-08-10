aa2d46d0598ba42e7f0d62391a6af3e909ab65df
Adam Joesten <adamjoesten@gmail.com>
2024-08-10 17:58:49 -0400

    <feat>: Git script working

--- scripts/machine-setup/personal/ubuntu/git.sh
+++ scripts/machine-setup/personal/ubuntu/git.sh
@@ -1,4 +1,4 @@
 # install git and git-lfs
-apt-get install git -y
-curl -s "https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh" | bash
-apt-get install git-lfs -y
+sudo apt-get install git -y
+curl -s "https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh" | sudo bash
+sudo apt-get install git-lfs -y
